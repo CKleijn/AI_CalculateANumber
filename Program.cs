0222f934ea3c9a3d41343a8d1da7fd157f5067e7
Calvin <cl.kleijn@student.avans.nl>
2024-06-18 22:45:03 +0200

    Add default targetStatic

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ int amountOfExecutionsStatic = 0;
 int amountOfErrorsStatic = 0;
 
 List<int> numbersStatic = [6, 10, 25, 75, 5, 50];
-int targetStatic = 728;
+int targetStatic = 741;
 
 while (amountOfLoopsStatic > 0)
 {
